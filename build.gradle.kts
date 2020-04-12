1f88aa5ef0ad290da26be5d96c513f1e3163b58f
Graham <gpe@openrs2.dev>
2020-04-12 09:24:48 +0100

    Output deobfuscator mapping files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,16 @@
 plugins {
     base
 }
+
+configure(subprojects) {
+    plugins.withType<JavaPlugin> {
+        dependencies {
+            val annotationProcessor by configurations
+            annotationProcessor(project(":deob-processor"))
+        }
+
+        tasks.named<JavaCompile>("compileJava") {
+            options.compilerArgs = listOf("-Amap=${project.rootDir}/data/deob-map/${project.name}.yaml")
+        }
+    }
+}
